1ef415c72585944ac06aa52679fc1560e59a5f26
jrymk <jerrymklu@gmail.com>
2023-01-13 18:33:30 +0800

    Change trigger to command hotkeys to prevent unwanted triggers

--- main.ts
+++ main.ts
@@ -1,12 +1,10 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
 interface TabKeyPluginSettings {
-	onlyDisableIndent: boolean,
 	indentsIfSelection: boolean
 }
 
 const DEFAULT_SETTINGS: TabKeyPluginSettings = {
-	onlyDisableIndent: false,
 	indentsIfSelection: true
 }
 
@@ -18,30 +16,39 @@ export default class TabKeyPlugin extends Plugin {
 
 		this.addSettingTab(new SettingTab(this.app, this));
 
-		this.registerDomEvent(document, 'keydown', (e: KeyboardEvent) => {
-			let view = this.app.workspace.getActiveViewOfType(MarkdownView);
-			if (!view)
-				return;
-			let editor = view.editor;
-			if (!editor.hasFocus())
-				return;
-			if (editor.getLine(editor.getCursor().line)[0] != '\t')
-				return;
-
-			if (e.code != 'Tab' || e.shiftKey)
-				return;
-
-			let cursorPos = editor.getCursor();
-			let cursorFrom = editor.getCursor("from");
-			let selection = editor.getSelection();
-			if (selection != '' && !this.settings.indentsIfSelection) {
-				editor.replaceSelection('\t');
-				cursorPos = cursorFrom;
-			}
-			if (selection == '' || !this.settings.indentsIfSelection)
-				editor.setLine(cursorPos.line, editor.getLine(cursorPos.line).substring(1, cursorPos.ch) + (selection == '' ? '\t' : '') + editor.getLine(cursorPos.line).substring(cursorPos.ch));
-			if (selection == '' || !this.settings.indentsIfSelection)
-				editor.setCursor(cursorPos);
+		this.addCommand({
+			id: 'obs-tab-tab-key',
+			name: 'Tab key trigger',
+			hotkeys: [
+				{
+					key: "Tab",
+					modifiers: []
+				}
+			],
+			editorCallback: (editor) => {
+				console.log("TAB");
+				let cursorPos = editor.getCursor();
+				let cursorFrom = editor.getCursor("from");
+				let selection = editor.getSelection();
+				if (selection != '' && !this.settings.indentsIfSelection) {
+					editor.replaceSelection('\t');
+					cursorPos = cursorFrom;
+				}
+				cursorPos.ch++;
+				cursorFrom.ch++;
+				if (selection == '' || !this.settings.indentsIfSelection) {
+					editor.setLine(cursorPos.line, editor.getLine(cursorPos.line).substring(0, cursorPos.ch - 1) + (selection == '' ? '\t' : '') + editor.getLine(cursorPos.line).substring(cursorPos.ch - 1));
+				}
+				else if (selection != '') {
+					for (let i = Math.min(cursorFrom.line, cursorPos.line); i <= Math.max(cursorFrom.line, cursorPos.line); i++)
+						editor.setLine(i, '\t' + editor.getLine(i));
+					editor.setSelection(cursorFrom, cursorPos);
+				}
+				if (!this.settings.indentsIfSelection && selection != '')
+					cursorPos.ch--;
+				if (selection == '' || !this.settings.indentsIfSelection)
+					editor.setCursor(cursorPos);
+			},
 		});
 	}
 
@@ -74,15 +81,6 @@ class SettingTab extends PluginSettingTab {
 		containerEl.createEl('i', { text: 'Restore tab key behaviour: tab key inserts a tab, the way it should be.' });
 		containerEl.createEl('br');
 
-		new Setting(containerEl)
-			.setName('Only disable indentation')
-			.setDesc('true: Will not insert tab character, will only prevent the line from indenting. ')
-			.addToggle(toggle => toggle
-				.setValue(this.plugin.settings.indentsIfSelection)
-				.onChange(async (value) => {
-					this.plugin.settings.indentsIfSelection = value;
-					await this.plugin.saveSettings();
-				}));
 		new Setting(containerEl)
 			.setName('Indents when selection is not empty')
 			.setDesc('true(default): Select some text and press tab key will indent the selected lines. Same behaviour as most IDEs. \nfalse: Selection will be replaced with one tab')
