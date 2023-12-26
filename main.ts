2b973798a41e11f1d08ccc951cbce47f973b5475
jerrymk <jerrymklu@gmail.com>
2023-12-27 05:37:21 +0800

    Added custom hotkey, Added Chinese localization

--- main.ts
+++ main.ts
@@ -1,18 +1,21 @@
-import { App, Editor, MarkdownView, KeymapContext, Hotkey, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-import { Extension, Prec } from '@codemirror/state';
-import { EditorView, keymap } from '@codemirror/view';
+import { Prec } from "@codemirror/state";
+import { EditorView, keymap } from "@codemirror/view";
+import { localization } from "localization";
+import { App, MarkdownView, Plugin, PluginSettingTab, Setting } from "obsidian";
 
 interface TabKeyPluginSettings {
-	indentsIfSelection: boolean,
-	indentsIfSelectionOnlyForMultipleLines: boolean,
-	useSpaces: boolean,
-	alignSpaces: boolean,
-	useHardSpace: boolean,
-	spacesCount: number,
-	allowException: boolean,
-	exceptionRegex: string,
-	useAdvancedTables: boolean,
-	useOutlinerBetterTab: boolean
+	indentsIfSelection: boolean;
+	indentsIfSelectionOnlyForMultipleLines: boolean;
+	useSpaces: boolean;
+	alignSpaces: boolean;
+	useHardSpace: boolean;
+	spacesCount: number;
+	allowException: boolean;
+	exceptionRegex: string;
+	useAdvancedTables: boolean;
+	useOutlinerBetterTab: boolean;
+	hotkey: string;
+	language: string;
 }
 
 const DEFAULT_SETTINGS: TabKeyPluginSettings = {
@@ -25,8 +28,10 @@ const DEFAULT_SETTINGS: TabKeyPluginSettings = {
 	allowException: true,
 	exceptionRegex: "^[\\s\u{00A0}]*(-|\\d+\\.)( \\[ \\])?\\s*$",
 	useAdvancedTables: true,
-	useOutlinerBetterTab: true
-}
+	useOutlinerBetterTab: true,
+	hotkey: "Tab",
+	language: "en-US",
+};
 
 export default class TabKeyPlugin extends Plugin {
 	settings: TabKeyPluginSettings;
@@ -37,60 +42,114 @@ export default class TabKeyPlugin extends Plugin {
 
 		let outlinerIndenting = false;
 
-		this.registerEditorExtension(Prec.highest(keymap.of(
-			[{
-				key: 'Tab',
-				run: (): boolean => {
-					if (outlinerIndenting)
-						return false;
-					const view = this.app.workspace.getActiveViewOfType(MarkdownView);
-					if (!view)
-						return false;
-					let editor = view.editor;
+		this.registerEditorExtension(
+			Prec.highest(
+				keymap.of([
+					{
+						key: this.settings.hotkey,
+						run: (): boolean => {
+							if (outlinerIndenting) return false;
+							const view =
+								this.app.workspace.getActiveViewOfType(
+									MarkdownView
+								);
+							if (!view) return false;
+							const editor = view.editor;
 
-					let cursorFrom = editor.getCursor("from");
-					let cursorTo = editor.getCursor("to");
-					let somethingSelected = (cursorFrom.line != cursorTo.line || cursorFrom.ch != cursorTo.ch);
-					const app = this.app as any;
+							const cursorFrom = editor.getCursor("from");
+							const cursorTo = editor.getCursor("to");
+							const somethingSelected =
+								cursorFrom.line != cursorTo.line ||
+								cursorFrom.ch != cursorTo.ch;
+							const app = this.app as any;
 
-					if (this.settings.useOutlinerBetterTab && RegExp("^[\\s]*(-|\\d+\\.)", 'u').test(editor.getLine(cursorFrom.line))) {
-						let prevLine = editor.getLine(cursorFrom.line);
-						outlinerIndenting = true;
-						app.commands.executeCommandById('obsidian-outliner:indent-list')
-						outlinerIndenting = false;
-						if (prevLine != editor.getLine(cursorFrom.line)) {
-							// outliner probably did its thing
-							return true;
-						}
-					}
+							if (
+								this.settings.useOutlinerBetterTab &&
+								RegExp("^[\\s]*(-|\\d+\\.)", "u").test(
+									editor.getLine(cursorFrom.line)
+								)
+							) {
+								const prevLine = editor.getLine(
+									cursorFrom.line
+								);
+								outlinerIndenting = true;
+								app.commands.executeCommandById(
+									"obsidian-outliner:indent-list"
+								);
+								outlinerIndenting = false;
+								if (
+									prevLine != editor.getLine(cursorFrom.line)
+								) {
+									// outliner probably did its thing
+									return true;
+								}
+							}
 
-					if (this.settings.useAdvancedTables && RegExp(`^\\|`, 'u').test(editor.getLine(cursorFrom.line))) {
-						app.commands.executeCommandById('table-editor-obsidian:next-cell');
-						return true;
-					}
-
-					if (somethingSelected && this.settings.indentsIfSelection && (!this.settings.indentsIfSelectionOnlyForMultipleLines || (cursorTo.line != cursorFrom.line))) {
-						editor.exec('indentMore');
-					} else {
-						let cursorFrom = editor.getCursor("from");
-						let tabStr = (this.settings.useSpaces ? (this.settings.useHardSpace ? ' ' : ' ').repeat(this.settings.alignSpaces ? (this.settings.spacesCount - cursorFrom.ch % this.settings.spacesCount) : (this.settings.spacesCount)) : '\t');
-
-						if (!somethingSelected && this.settings.allowException) {
-							if (RegExp(this.settings.exceptionRegex, 'u').test(editor.getLine(cursorFrom.line))) {
-								editor.exec('indentMore');
+							if (
+								this.settings.useAdvancedTables &&
+								RegExp(`^\\|`, "u").test(
+									editor.getLine(cursorFrom.line)
+								)
+							) {
+								app.commands.executeCommandById(
+									"table-editor-obsidian:next-cell"
+								);
 								return true;
 							}
-						}
 
-						// insert tab
-						editor.replaceSelection(tabStr);
-						editor.setCursor({ line: cursorFrom.line, ch: cursorFrom.ch + tabStr.length });
-					}
-					return true;
-				},
-				preventDefault: true,
-			}])
-		));
+							if (
+								somethingSelected &&
+								this.settings.indentsIfSelection &&
+								(!this.settings
+									.indentsIfSelectionOnlyForMultipleLines ||
+									cursorTo.line != cursorFrom.line)
+							) {
+								editor.exec("indentMore");
+							} else {
+								const cursorFrom = editor.getCursor("from");
+								const tabStr = this.settings.useSpaces
+									? (this.settings.useHardSpace
+											? " "
+											: " "
+									  ).repeat(
+											this.settings.alignSpaces
+												? this.settings.spacesCount -
+														(cursorFrom.ch %
+															this.settings
+																.spacesCount)
+												: this.settings.spacesCount
+									  )
+									: "\t";
+
+								if (
+									!somethingSelected &&
+									this.settings.allowException
+								) {
+									if (
+										RegExp(
+											this.settings.exceptionRegex,
+											"u"
+										).test(editor.getLine(cursorFrom.line))
+									) {
+										editor.exec("indentMore");
+										return true;
+									}
+								}
+
+								// insert tab
+								editor.replaceSelection(tabStr);
+								editor.setCursor({
+									line: cursorFrom.line,
+									ch: cursorFrom.ch + tabStr.length,
+								});
+							}
+							return true;
+						},
+						preventDefault: true,
+					},
+				])
+			)
+		);
 	}
 
 	createKeymapRunCallback() {
@@ -99,11 +158,14 @@ export default class TabKeyPlugin extends Plugin {
 		};
 	}
 
-	onunload() {
-	}
+	onunload() {}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
 
 	async saveSettings() {
@@ -122,133 +184,281 @@ class SettingTab extends PluginSettingTab {
 	display(): void {
 		const { containerEl } = this;
 		containerEl.empty();
-		containerEl.createEl('h5', { text: 'Obsidian Tab Key Plugin' });
-		containerEl.createEl('i', { text: 'Restore tab key behaviour: tab key inserts a tab, the way it should be.' });
-		containerEl.createEl('br');
-		containerEl.createEl('br');
-
-		containerEl.createEl('h2', { text: 'Tab or Space Settings' });
+		containerEl.createEl("h3", {
+			text: localization["title"][this.plugin.settings.language],
+		});
+		containerEl.createEl("i", {
+			text: localization["description"][this.plugin.settings.language],
+		});
+		containerEl.createEl("br");
+		containerEl.createEl("br");
 
 		new Setting(containerEl)
-			.setName('Use spaces instead of tab')
-			.setDesc('false(default): Insert tab (\\t) when tab key is pressed. true: Insert spaces (    ) when tab key is pressed.')
-			.addToggle(toggle => toggle
-				.setValue(this.plugin.settings.useSpaces)
-				.onChange(async (value) => {
-					this.plugin.settings.useSpaces = value;
-					this.display(); // refresh display
-					await this.plugin.saveSettings();
-				}));
+			.setName(localization["language"][this.plugin.settings.language])
+			.setDesc(
+				localization["languageDesc"][this.plugin.settings.language]
+			)
+			.addDropdown((toggle) =>
+				toggle
+					.addOptions({
+						"en-US": "English (US)",
+						"zh-CN": "简体中文",
+						"zh-TW": "繁體中文",
+					})
+					.setValue(this.plugin.settings.language)
+					.onChange(async (value) => {
+						this.plugin.settings.language = value;
+						await this.plugin.saveSettings();
+						this.display(); // refresh display
+					})
+			);
+
+		containerEl.createEl("h5", {
+			text: localization["tabOrSpace"][this.plugin.settings.language],
+		});
+
+		new Setting(containerEl)
+			.setName(
+				localization["useSpacesInsteadOfTab"][
+					this.plugin.settings.language
+				]
+			)
+			.setDesc(
+				localization["useSpacesInsteadOfTabDesc"][
+					this.plugin.settings.language
+				]
+			)
+			.addToggle((toggle) =>
+				toggle
+					.setValue(this.plugin.settings.useSpaces)
+					.onChange(async (value) => {
+						this.plugin.settings.useSpaces = value;
+						this.display(); // refresh display
+						await this.plugin.saveSettings();
+					})
+			);
 
 		if (this.plugin.settings.useSpaces) {
 			new Setting(containerEl)
-				.setName('Use hard spaces')
-				.setDesc('If "Indent using tabs" is false, space will be used to indent. If "Use hard spaces" is off, a normal space character will be used. Notice that with Markdown, repeated normal spaces will be rendered as one. Turn this option on to use hard spaces (U+00A0), which will not be truncated after Markdown render. To indent stuff in the processed Markdown output, move your cursor to the begin and press tab (indenting won\'t insert hard spaces)')
-				.addToggle(toggle => toggle
-					.setValue(this.plugin.settings.useHardSpace)
-					.onChange(async (value) => {
-						this.plugin.settings.useHardSpace = value;
-						await this.plugin.saveSettings();
-					}));
+				.setName(
+					localization["useHardSpaces"][this.plugin.settings.language]
+				)
+				.setDesc(
+					localization["useHardSpacesDesc"][
+						this.plugin.settings.language
+					]
+				)
+				.addToggle((toggle) =>
+					toggle
+						.setValue(this.plugin.settings.useHardSpace)
+						.onChange(async (value) => {
+							this.plugin.settings.useHardSpace = value;
+							await this.plugin.saveSettings();
+						})
+				);
 
 			new Setting(containerEl)
-				.setName('Space count')
-				.setDesc('The number of spaces or hard spaces inserted when tab key is pressed. default: 4')
-				.addSlider(slider => slider
-					.setValue(this.plugin.settings.spacesCount)
-					.setLimits(2, 8, 1)
-					.setDynamicTooltip()
-					.onChange(async (value) => {
-						this.plugin.settings.spacesCount = value;
-						await this.plugin.saveSettings();
-					}));
+				.setName(
+					localization["spaceCount"][this.plugin.settings.language]
+				)
+				.setDesc(
+					localization["spaceCountDesc"][
+						this.plugin.settings.language
+					]
+				)
+				.addSlider((slider) =>
+					slider
+						.setValue(this.plugin.settings.spacesCount)
+						.setLimits(2, 8, 1)
+						.setDynamicTooltip()
+						.onChange(async (value) => {
+							this.plugin.settings.spacesCount = value;
+							await this.plugin.saveSettings();
+						})
+				);
 
 			new Setting(containerEl)
-				.setName('Align spaces (just like how tabs behave)')
-				.setDesc('At space count of 4, pressing tab after "abc" inserts one space, "abcde" inserts 3, so the end position after pressing tab is always an integer multiple of the space count.')
-				.addToggle(toggle => toggle
-					.setValue(this.plugin.settings.alignSpaces)
-					.onChange(async (value) => {
-						this.plugin.settings.alignSpaces = value;
-						await this.plugin.saveSettings();
-					}));
+				.setName(
+					localization["alignSpaces"][this.plugin.settings.language]
+				)
+				.setDesc(
+					localization["alignSpacesDesc"][
+						this.plugin.settings.language
+					]
+				)
+				.addToggle((toggle) =>
+					toggle
+						.setValue(this.plugin.settings.alignSpaces)
+						.onChange(async (value) => {
+							this.plugin.settings.alignSpaces = value;
+							await this.plugin.saveSettings();
+						})
+				);
 		}
 
-		containerEl.createEl('h2', { text: 'Tab Key Behaviour' });
+		containerEl.createEl("h5", {
+			text: localization["tabKeyBehavior"][this.plugin.settings.language],
+		});
 
 		new Setting(containerEl)
-			.setName('Indents when selection is not empty')
-			.setDesc('true(default): Select some text and press tab key will indent the selected lines. Same behaviour as most IDEs. \nfalse: Selection will be replaced with one tab')
-			.addToggle(toggle => toggle
-				.setValue(this.plugin.settings.indentsIfSelection)
-				.onChange(async (value) => {
-					this.plugin.settings.indentsIfSelection = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName(
+				localization["indentWhenSelectionNotEmpty"][
+					this.plugin.settings.language
+				]
+			)
+			.setDesc(
+				localization["indentWhenSelectionNotEmptyDesc"][
+					this.plugin.settings.language
+				]
+			)
+			.addToggle((toggle) =>
+				toggle
+					.setValue(this.plugin.settings.indentsIfSelection)
+					.onChange(async (value) => {
+						this.plugin.settings.indentsIfSelection = value;
+						await this.plugin.saveSettings();
+					})
+			);
 
 		if (this.plugin.settings.indentsIfSelection) {
 			new Setting(containerEl)
-				.setName('Indents only when selection contains multiple lines')
-				.setDesc('true(default): If the selection lies within one line, a tab (or spaces) will replace the selection instead')
-				.addToggle(toggle => toggle
-					.setValue(this.plugin.settings.indentsIfSelectionOnlyForMultipleLines)
-					.onChange(async (value) => {
-						this.plugin.settings.indentsIfSelectionOnlyForMultipleLines = value;
-						await this.plugin.saveSettings();
-					}));
+				.setName(
+					localization["indentOnlySelectionMultipleLine"][
+						this.plugin.settings.language
+					]
+				)
+				.setDesc(
+					localization["indentOnlySelectionMultipleLineDesc"][
+						this.plugin.settings.language
+					]
+				)
+				.addToggle((toggle) =>
+					toggle
+						.setValue(
+							this.plugin.settings
+								.indentsIfSelectionOnlyForMultipleLines
+						)
+						.onChange(async (value) => {
+							this.plugin.settings.indentsIfSelectionOnlyForMultipleLines =
+								value;
+							await this.plugin.saveSettings();
+						})
+				);
 		}
 
 		new Setting(containerEl)
-			.setName('Allow exceptions for indenting')
-			.setDesc('Indent line even when the selection is empty when the line matches the regex')
-			.addToggle(toggle => toggle
-				.setValue(this.plugin.settings.allowException)
-				.onChange(async (value) => {
-					this.plugin.settings.allowException = value;
-					this.display(); // refresh display
-					await this.plugin.saveSettings();
-				}));
+			.setName(
+				localization["allowException"][this.plugin.settings.language]
+			)
+			.setDesc(
+				localization["allowExceptionDesc"][
+					this.plugin.settings.language
+				]
+			)
+			.addToggle((toggle) =>
+				toggle
+					.setValue(this.plugin.settings.allowException)
+					.onChange(async (value) => {
+						this.plugin.settings.allowException = value;
+						this.display(); // refresh display
+						await this.plugin.saveSettings();
+					})
+			);
 		if (this.plugin.settings.allowException) {
 			new Setting(containerEl)
-				.setName('Exception regex')
-				.setDesc('Default: Indents regardless in lists (zero or more whitespaces, followed by - or number. then optionally a checkbox and then a space). Remove the trailing $ to enable indentation in non-empty lists')
-				.addText(textbox => textbox
-					.setValue(this.plugin.settings.exceptionRegex)
-					.setPlaceholder('Regex')
-					.onChange(async (value) => {
-						this.plugin.settings.exceptionRegex = value;
-						await this.plugin.saveSettings();
-					}))
-				.addExtraButton(button => button
-					.setIcon('rotate-ccw')
-					.onClick(async () => {
-						this.plugin.settings.exceptionRegex = '^[\\s\u{00A0}]*(-|\\d+\\.)( \\[ \\])?\\s*$'
+				.setName(
+					localization["exceptionRegex"][
+						this.plugin.settings.language
+					]
+				)
+				.setDesc(
+					localization["exceptionRegexDesc"][
+						this.plugin.settings.language
+					]
+				)
+				.addText((textbox) =>
+					textbox
+						.setValue(this.plugin.settings.exceptionRegex)
+						.setPlaceholder("Regex")
+						.onChange(async (value) => {
+							this.plugin.settings.exceptionRegex = value;
+							await this.plugin.saveSettings();
+						})
+				)
+				.addExtraButton((button) =>
+					button.setIcon("rotate-ccw").onClick(async () => {
+						this.plugin.settings.exceptionRegex =
+							"^[\\s\u{00A0}]*(-|\\d+\\.)( \\[ \\])?\\s*$";
 						this.display();
 						await this.plugin.saveSettings();
-					}))
+					})
+				);
 		}
 
-		containerEl.createEl('h2', { text: 'Plugin Compatibility' });
+		containerEl.createEl("h5", {
+			text: localization["pluginCompatibility"][
+				this.plugin.settings.language
+			],
+		});
 
 		new Setting(containerEl)
-			.setName('Use with Advanced Tables plugin')
-			.setDesc('Creates a new table or go to next cell when cursor is in a table')
-			.addToggle(toggle => toggle
-				.setValue(this.plugin.settings.useAdvancedTables)
-				.onChange(async (value) => {
-					this.plugin.settings.useAdvancedTables = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName(
+				localization["advancedTables"][this.plugin.settings.language]
+			)
+			.setDesc(
+				localization["advancedTablesDesc"][
+					this.plugin.settings.language
+				]
+			)
+			.addToggle((toggle) =>
+				toggle
+					.setValue(this.plugin.settings.useAdvancedTables)
+					.onChange(async (value) => {
+						this.plugin.settings.useAdvancedTables = value;
+						await this.plugin.saveSettings();
+					})
+			);
 
 		new Setting(containerEl)
-			.setName('Use with Obsidian Outliner plugin')
-			.setDesc('Try execute Outliner indent operation when tab is pressed, if nothing changed, use default Restore Tab Key plugin behavior')
-			.addToggle(toggle => toggle
-				.setValue(this.plugin.settings.useOutlinerBetterTab)
-				.onChange(async (value) => {
-					this.plugin.settings.useOutlinerBetterTab = value;
+			.setName(localization["outliner"][this.plugin.settings.language])
+			.setDesc(
+				localization["outlinerDesc"][this.plugin.settings.language]
+			)
+			.addToggle((toggle) =>
+				toggle
+					.setValue(this.plugin.settings.useOutlinerBetterTab)
+					.onChange(async (value) => {
+						this.plugin.settings.useOutlinerBetterTab = value;
+						await this.plugin.saveSettings();
+					})
+			);
+
+		containerEl.createEl("h5", {
+			text: localization["additional"][this.plugin.settings.language],
+		});
+
+		new Setting(containerEl)
+			.setName(
+				localization["customHotkey"][this.plugin.settings.language]
+			)
+			.setDesc(
+				localization["customHotkeyDesc"][this.plugin.settings.language]
+			)
+			.addText((textbox) =>
+				textbox
+					.setValue(this.plugin.settings.hotkey)
+					.setPlaceholder("Ctrl-Alt-Space")
+					.onChange(async (value) => {
+						this.plugin.settings.hotkey = value;
+						await this.plugin.saveSettings();
+					})
+			)
+			.addExtraButton((button) =>
+				button.setIcon("rotate-ccw").onClick(async () => {
+					this.plugin.settings.hotkey = "Tab";
+					this.display();
 					await this.plugin.saveSettings();
-				}));
+				})
+			);
 	}
-
 }
